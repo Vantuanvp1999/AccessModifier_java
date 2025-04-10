bca31d303ead15333d39c9efba66788ed9c96873
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-10 11:29:51 +0700

    addbt1

---
+++ src/Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        TestCircle circle = new TestCircle(2.5);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getRadius());
+    }
+}
---
+++ src/TestCircle.java
@@ -0,0 +1,19 @@
+public class TestCircle {
+    private double radius = 1.0;
+    private String color = "red";
+    public TestCircle() {}
+    public TestCircle(double radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(){
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+
+    }
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+}
